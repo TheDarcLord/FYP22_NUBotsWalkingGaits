8a7b1a63c9d56b9511ccf244ac7861b0d2c0d7ce
Darcy Byrne <darcybyrne3@gmail.com>
2022-04-16 20:08:59 +1000

    Implement basic video writing
    ...
    https://au.mathworks.com/matlabcentral/answers/384848-get-figures-and-use-them-to-build-a-video-avi

--- MatLab/TwoDimensions/main.m
+++ MatLab/TwoDimensions/main.m
@@ -160,6 +160,21 @@ for i=1:length(model.tspan)
     axis([-1 1 -0.5 2 0 2]);
     view(135,35);
     %view(90,0); % -> 2D
-    pause(0.01)
+    F(i) = getframe(gcf);
+    drawnow
 end
 
+% create the video writer with 1 fps
+writerObj = VideoWriter('myVideo.avi');
+writerObj.FrameRate = 10;
+% set the seconds per image
+% open the video writer
+open(writerObj);
+% write the frames to the video
+for i=1:length(F)
+    % convert the image to a frame
+    frame = F(i) ;    
+    writeVideo(writerObj, frame);
+end
+% close the writer object
+close(writerObj);
