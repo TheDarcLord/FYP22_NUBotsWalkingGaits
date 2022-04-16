d467f43249390a60267f636294221e3c8e9ae385
Darcy Byrne <darcybyrne3@gmail.com>
2022-04-16 18:20:23 +1000

    Add Rudimentary CoM Function
    Rudimentary in;
    - Being from first principles,
    - Disregarding the Feet
    - Masses being positioned ~Halfway along each link

--- MatLab/TwoDimensions/k.m
+++ MatLab/TwoDimensions/k.m
@@ -1,19 +1,20 @@
-function [xe, T16, Transforms] = k(q, params)
+function [xe, T16, HTs] = k(q, params)
 % k(q)  [2D Model] Forward Kinematic Model - FKM
 %       
 %       Returns: [T16 Transforms] for an array of 'q'
 %       T16:        Transform from ONE to END EFFECTOR
 %       Transforms: All other Homogenous Transforms
-    t1     = q(1); % θ₁
-    t2     = q(2); % θ₂
-    t3     = q(3); % θ₂
-    t4     = q(4); % θ₃
-    t5     = q(5); % θ₃
-    t6     = q(6); % θ₃
 
-    L1     = params.L1;
-    L2     = params.L2;
-    H      = params.HipWidth;
+    t1     = q(1); % θ₁     ankle
+    t2     = q(2); % θ₂     knee
+    t3     = q(3); % θ₃     hip
+    t4     = q(4); % θ₄     hip
+    t5     = q(5); % θ₅     knee
+    t6     = q(6); % θ₆     ankle
+
+    L_lower     = params.fibula;
+    L_upper     = params.femur;
+    H           = params.HipWidth;
 
     RZ    = @(psi) ...
             [cos(psi) -sin(psi)  0  0;
@@ -24,8 +25,8 @@ function [xe, T16, Transforms] = k(q, params)
             [eye(3)     [x;y;z];
              zeros(1,3)      1];
 
-    T16 = RZ(t1)*T(0,L1,0)*RZ(t2)*T(0,L2,0)*RZ(t3)*T(0,0,-H)* ... TO HIP
-          RZ(t4)*T(0,-L2,0)*RZ(t5)*T(0,-L1,0)*RZ(t6);
+    T16 = RZ(t1)*T(0,L_lower,0)*RZ(t2)*T(0,L_upper,0)*RZ(t3)*T(0,0,-H)* ... TO HIP
+          RZ(t4)*T(0,-L_upper,0)*RZ(t5)*T(0,-L_lower,0)*RZ(t6);
     
     % Ψ = atan2( R₃₂, R₃₃)
     % θ = atan2( R₂₁, R₁₁)
@@ -50,16 +51,16 @@ function [xe, T16, Transforms] = k(q, params)
           theta;      % θ
           psi];       % Ψ
     
-    
-    Transforms.A12 = RZ(t1)*T(0,L1,0);
-    Transforms.A23 = RZ(t2)*T(0,L2,0);
-    Transforms.A34 = RZ(t3)*T(0,0,-H);
-    Transforms.A45 = RZ(t4)*T(0,-L2,0);
-    Transforms.A56 = RZ(t5)*T(0,-L1,0);
-
-    Transforms.A13 = Transforms.A12*Transforms.A23;
-    Transforms.A14 = Transforms.A13*Transforms.A34;
-    Transforms.A15 = Transforms.A14*Transforms.A45;
-    Transforms.A16 = Transforms.A15*Transforms.A56;
+    HTs.A01 = T(0,0,0);
+        HTs.A12 = RZ(t1)*T(0,L_lower,0);
+        HTs.A23 = RZ(t2)*T(0,L_upper,0);
+        HTs.A34 = RZ(t3)*T(0,0,-H);
+        HTs.A45 = RZ(t4)*T(0,-L_upper,0);
+        HTs.A56 = RZ(t5)*T(0,-L_lower,0);
+    HTs.A02 = HTs.A01*HTs.A12;
+    HTs.A03 = HTs.A02*HTs.A23;
+    HTs.A04 = HTs.A03*HTs.A34;
+    HTs.A05 = HTs.A04*HTs.A45;
+    HTs.A06 = HTs.A05*HTs.A56;
 
 end
--- MatLab/TwoDimensions/main.m
+++ MatLab/TwoDimensions/main.m
@@ -21,9 +21,16 @@ params.orientation  = 1;
 model.tspan         = 0:0.1:2;
 model.q             = zeros(6,length(model.tspan)); % Making Space
 model.xe            = zeros(6,length(model.tspan)); % Making Space
-params.L1           = 0.5;
-params.L2           = 0.5;
+params.fibula       = 0.5;
+params.femur        = 0.5;
 params.HipWidth     = 0.3;
+
+% Masses
+params.mass.femur   = 1;    % Thigh Bone
+params.mass.fibula  = 1;    % Paired with `tibia`
+params.mass.joint   = 0.5;  % Knee Bone / Joints
+params.mass.pelvis  = 1.5;  % Waist 
+
 
 %% Initial Position & Orientation
 
@@ -95,47 +102,48 @@ figure('Name','Animation')
     %[xe, T03, Transforms] = k(model.q(:,i), params);
     % [xe, T16, Transforms] = k(q0, params);
     qStar = k_Inv(q0, xe0, params);
-    [xe, T16, Transforms] = k(qStar, params);
+    [xe, T16, HomegeneousTransforms] = k(qStar, params);
 
     clf
     hold on
     % Z X Y
-    %   ZERO
+    % ZERO
     plot3([0 3], [0 0], [0 0],'r', 'LineWidth',0.5); % Z
     plot3([0 0], [0 3], [0 0],'g', 'LineWidth',0.5); % X
     plot3([0 0], [0 0], [0 3],'b', 'LineWidth',0.5); % Y
     legend('+Z','+X','+Y','Autoupdate','off');
     % ONE
-    r01 = [0;0;0];
+    r01 = HomegeneousTransforms.A01(1:3,4);
     plot3(r01(3), r01(1), r01(2), 'gx', 'LineWidth',2);         % JOINT ONE
     % TWO
-    r12 = Transforms.A12(1:3,4);
-    plot3([r01(3) r12(3)], [r01(1) r12(1)], [r01(2) r12(2)],... % LINK
+    r02 = HomegeneousTransforms.A02(1:3,4);
+    plot3([r01(3) r02(3)], [r01(1) r02(1)], [r01(2) r02(2)],... % LINK
     'k', 'LineWidth',2);
-    plot3(r12(3), r12(1), r12(2) ,'gx', 'LineWidth',2);         % JOINT TWO
+    plot3(r02(3), r02(1), r02(2) ,'gx', 'LineWidth',2);         % JOINT TWO
     % THREE
-    r13 = Transforms.A13(1:3,4);
-    plot3([r12(3) r13(3)], [r12(1) r13(1)], [r12(2) r13(2)],... % LINK
+    r03 = HomegeneousTransforms.A03(1:3,4);
+    plot3([r02(3) r03(3)], [r02(1) r03(1)], [r02(2) r03(2)],... % LINK
     'k', 'LineWidth',2);
-    plot3(r13(3), r13(1), r13(2), 'gx', 'LineWidth',2);         % JOINT THREE
+    plot3(r03(3), r03(1), r03(2), 'gx', 'LineWidth',2);         % JOINT THREE
     % FOUR
-    r14 = Transforms.A14(1:3,4);
-    plot3([r13(3) r14(3)], [r13(1) r14(1)], [r13(2) r14(2)],... % LINK
+    r04 = HomegeneousTransforms.A04(1:3,4);
+    plot3([r03(3) r04(3)], [r03(1) r04(1)], [r03(2) r04(2)],... % LINK
     'k', 'LineWidth',2);
-    plot3(r14(3), r14(1), r14(2), 'bx', 'LineWidth',2);         % JOINT FOUR
+    plot3(r04(3), r04(1), r04(2), 'bx', 'LineWidth',2);         % JOINT FOUR
     % FIVE
-    r15 = Transforms.A15(1:3,4);
-    plot3([r14(3) r15(3)], [r14(1) r15(1)], [r14(2) r15(2)],... % LINK
+    r05 = HomegeneousTransforms.A05(1:3,4);
+    plot3([r04(3) r05(3)], [r04(1) r05(1)], [r04(2) r05(2)],... % LINK
     'k', 'LineWidth',2);
-    plot3(r15(3), r15(1), r15(2), 'bx', 'LineWidth',2);         % JOINT FIVE
+    plot3(r05(3), r05(1), r05(2), 'bx', 'LineWidth',2);         % JOINT FIVE
     % SIX 
-    r16 = Transforms.A16(1:3,4);
-    plot3([r15(3) r16(3)], [r15(1) r16(1)], [r15(2) r16(2)],... % LINK
+    r06 = HomegeneousTransforms.A06(1:3,4);
+    plot3([r05(3) r06(3)], [r05(1) r06(1)], [r05(2) r06(2)],... % LINK
     'k', 'LineWidth',2);
-    plot3(r16(3), r16(1), r16(2), 'bx', 'LineWidth',2);         % JOINT SIX
+    plot3(r06(3), r06(1), r06(2), 'bx', 'LineWidth',2);         % JOINT SIX
     
-
-    %plot3([0 r12(1)], [0 r12(2)], [0 r12(3)],'k', 'LineWidth',1);
+    r0CoM = rCoM(qStar,params);
+    plot3(r0CoM(3),r0CoM(1),0, 'rx', 'LineWidth',2);
+    %plot3([0 r02(1)], [0 r02(2)], [0 r02(3)],'k', 'LineWidth',1);
     
     %        X    Y   Z
     %        Z    X   Y
