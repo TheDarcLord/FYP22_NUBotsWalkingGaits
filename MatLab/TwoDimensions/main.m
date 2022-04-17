05d5aac2ccbbb1d16aad0025f77c8f7f9981f956
Darcy Byrne <darcybyrne3@gmail.com>
2022-04-17 15:06:34 +1000

    Debug Rudimentary Trajectory Generation
    - Fix bugs associated with rudimentary trajectory generation
    - Implement Quintic Spline Trajectories
    - Change video output from `.avi` to `.mp4`
    - Plot foot trajectory

--- MatLab/TwoDimensions/main.m
+++ MatLab/TwoDimensions/main.m
@@ -15,10 +15,11 @@ clc
 % R₃₁: -sθ₂
 % R₃₂:  0
 % R₃₃:  cθ₂
-
+    
 %% Setup
+params.framerate    = 20;
 params.orientation  = 1;
-model.tspan         = 0:0.1:5;
+model.tspan         = 0:(1 / params.framerate):6;
 model.q             = zeros(6,length(model.tspan)); % Making Space
 model.xe            = zeros(6,length(model.tspan)); % Making Space
 params.fibula       = 0.5;
@@ -50,43 +51,77 @@ model.xe0 = [0; % X
 
 
 %% Trajectory
-D = diag(1:3,-1);   % Special D - Diag Matrix
+D = diag(1:5,-1);   % Special D - Diag Matrix
 
-q0 = [0 0;  % qX vX
-      0 0;  % qY vY
-   -0.3 0]; % qZ vZ
-t0 = 1;
-tt0 = t0.^(0:3).';
-T0 = [tt0, D*tt0];
+q0 = [0.00  0.00  0.00;  % qX vX aX
+      0.00  0.00  0.00;  % qY vY aY
+     -0.30  0.00  0.00]; % qZ vZ aZ
+t0 = 0;
+tt0 = t0.^(0:5).';
+T0 = [tt0, D*tt0, D^2*tt0];
 
-q1 = [0 0;  % qX vX
-      0 0;  % qY vY
-   -0.3 0]; % qZ vZ
-t1 = 2;
-tt1 = t1.^(0:3).';
-T1 = [tt1, D*tt1];
+q1 = [0.00  0.00  0.00;  % qX vX aX
+      0.00  0.00  0.00;  % qY vY aY
+     -0.30  0.00  0.00]; % qZ vZ aZ
+t1 = 1;
+tt1 = t1.^(0:5).';
+T1 = [tt1, D*tt1, D^2*tt1];
 
-q2 = [0.25   0;  % qX vX
-      0.1    0;  % qY vY
-     -0.3   0]; % qZ vZ
+q2 = [0.25  0.00  0.00;  % qX vX aX
+      0.25  0.00  0.00;  % qY vY aY
+     -0.30  0.00  0.00]; % qZ vZ aZ
 t2 = 3;
-tt2 = t2.^(0:3).';
-T2 = [tt2, D*tt2];
+tt2 = t2.^(0:5).';
+T2 = [tt2, D*tt2, D^2*tt2];
 
-q3 = [0.5 0;  % qX vX
-      0 0;   % qY vY
-     -0.3  0]; % qZ vZ
+q3 = [0.50  0.00  0.00;  % qX vX aX
+      0.00  0.00  0.00;  % qY vY aY
+     -0.30  0.00  0.00]; % qZ vZ aZ
 t3 = 4;
-tt3 = t3.^(0:3).';
-T3 = [tt3, D*tt3];
+tt3 = t3.^(0:5).';
+T3 = [tt3, D*tt3, D^2*tt3];
 
-C = [q0 q1 q2 q3]/[T0 T1 T2 T3];
+q4 = [0.50  0.00  0.00;  % qX vX aX
+      0.00  0.00  0.00;  % qY vY aY
+     -0.30  0.00  0.00]; % qZ vZ aZ
+t4 = 5;
+tt4 = t4.^(0:5).';
+T4 = [tt4, D*tt4, D^2*tt4];
+
+q5 = [0.50  0.00  0.00;  % qX vX aX
+      0.00  0.00  0.00;  % qY vY aY
+     -0.30  0.00  0.00]; % qZ vZ aZ
+t5 = 6;
+tt5 = t5.^(0:5).';
+T5 = [tt5, D*tt5, D^2*tt5];
+
+C = [q0 q1 q2 q3 q4 q5]/[T0 T1 T2 T3 T4 T5];
 
 % Evaluate postions and time derivatives
-tt = model.tspan.^((0:3).');
+tt = model.tspan.^((0:5).');
 Q = C*tt;    % Targeting Postions First ... Worry about Velocities later
 V = C*D*tt;
 A = C*D^2*tt;
+
+% DEBUG
+% figure('Name','Trajectory')
+% for i=1:length(model.tspan)
+%         clf
+%         hold on
+%         plot3([0 2], [0 0], [0 0],'r', 'LineWidth',0.5); % Z
+%         plot3([0 0], [0 2], [0 0],'g', 'LineWidth',0.5); % X
+%         plot3([0 0], [0 0], [0 2],'b', 'LineWidth',0.5); % Y
+%         plot3(Q(3,1:i),Q(1,1:i),Q(2,1:i), 'k-','LineWidth', 0.5);
+%         % Way Points
+%         plot3(q0(3,1),q0(1,1),q0(2,1),'rx','LineWidth',0.5);
+%         plot3(q1(3,1),q1(1,1),q1(2,1),'gx','LineWidth',0.5);
+%         plot3(q2(3,1),q2(1,1),q2(2,1),'bx','LineWidth',0.5);
+%         plot3(q3(3,1),q3(1,1),q3(2,1),'cx','LineWidth',0.5);
+%         legend('+Z','+X','+Y','Trajectory','WP0','WP1','WP2','WP3','Autoupdate','off');
+%         %view(135,35);
+%         view(90,0);
+%         pause(0.01)
+% end
 
 %% LOOP
 
@@ -103,6 +138,7 @@ end
 
 %% Graph
 figure('Name','Animation')
+
 for i=1:length(model.tspan)
     
     [~, ~, HomegeneousTransforms] = k(model.q(:,i), params);
@@ -114,15 +150,17 @@ for i=1:length(model.tspan)
 
     clf
     hold on
-    
-    txt = {'Plotted Data:','y = sin(x)'};
-    text(0,1.5,1.5,txt)
+    grid on
+    title("Step One")
+    txt = " Time: " + num2str(model.tspan(i)) + " sec";
+    text(0,2,2,txt)
     % Z X Y
     % ZERO
     plot3([0 3], [0 0], [0 0],'r', 'LineWidth',0.5); % Z
     plot3([0 0], [0 3], [0 0],'g', 'LineWidth',0.5); % X
     plot3([0 0], [0 0], [0 3],'b', 'LineWidth',0.5); % Y
-    legend('+Z','+X','+Y','Autoupdate','off');
+    plot3(Q(3,1:i),Q(1,1:i),Q(2,1:i), 'k-','LineWidth', 0.5)
+    legend('+Z','+X','+Y', 'Trajectory','Autoupdate','off');
     % ONE
     r01 = HomegeneousTransforms.A01(1:3,4);
     plot3(r01(3), r01(1), r01(2), 'gx', 'LineWidth',2);         % JOINT ONE
@@ -160,20 +198,20 @@ for i=1:length(model.tspan)
     axis([-1 1 -0.5 2 0 2]);
     view(135,35);
     %view(90,0); % -> 2D
-    F(i) = getframe(gcf);
+    FRAME(i) = getframe(gcf);
     drawnow
 end
 
 % create the video writer with 1 fps
-writerObj = VideoWriter('myVideo.avi');
-writerObj.FrameRate = 10;
+writerObj = VideoWriter('2D_Step.mp4','MPEG-4');
+writerObj.FrameRate = params.framerate;
 % set the seconds per image
 % open the video writer
-open(writerObj);
+open(writerObj);                        
 % write the frames to the video
-for i=1:length(F)
+for i=1:length(FRAME)
     % convert the image to a frame
-    frame = F(i) ;    
+    frame = FRAME(i) ;    
     writeVideo(writerObj, frame);
 end
 % close the writer object
