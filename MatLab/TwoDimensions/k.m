8575195dc1a1729212aad2760734d389387dcb96
Darcy Byrne <darcybyrne3@gmail.com>
2022-04-17 19:04:02 +1000

    Remove `symbolic.m`
    - Further simplify `k(q)`, the FKM
    - Update `k(q)` function information
    - Time `main.m` execution
    - Remove Verbose Video details
    - Remove Trajectory Debug
    - Lower Crouch

--- MatLab/TwoDimensions/k.m
+++ MatLab/TwoDimensions/k.m
@@ -1,27 +1,28 @@
 function [xe, T16, HTs] = k(q, params)
 % k(q)  [2D Model] Forward Kinematic Model - FKM
 %       
-%       Returns: [T16 Transforms] for an array of 'q'
+%       Returns:    [xe, T16, Transforms] for an array of 'q'
+%       xe:         End Effector Pose [X Y Z ϕ θ Ψ]'
 %       T16:        Transform from ONE to END EFFECTOR
 %       Transforms: All other Homogenous Transforms
 
-    t1          = q(1);          % θ₁     ankle
-    t2          = q(2);          % θ₂     knee
-    t3          = q(3);          % θ₃     hip
-    t4          = q(4);          % θ₄     hip
-    t5          = q(5);          % θ₅     knee
-    t6          = q(6);          % θ₆     ankle
+    %% LINK VARIABLES
+    Ll     = params.fibula; % Lower Leg
+    Lu     = params.femur;  % Upper Leg
+    H      = params.HipWidth;
+
+    %% JOINT VARIABLES
     sigT        = sum(q);        % Σθᵢ    i=1:6
     sigT15      = sum(q(1:5));   % Σθᵢ    i=1:5
     sigT14      = sum(q(1:4));   % Σθᵢ    i=1:4
+    sigT13      = sum(q(1:3));   % Σθᵢ    i=1:3
     sigT12      = sum(q(1:2));   % Σθᵢ    i=1:2
 
-    L_lower     = params.fibula;
-    L_upper     = params.femur;
-    H           = params.HipWidth;
-
-    T16x = L_upper*sin(sigT14) - L_upper*sin(sigT12) - L_lower*sin(t1) + L_lower*sin(sigT15);
-    T16y = L_upper*cos(sigT12) - L_upper*cos(sigT14) + L_lower*cos(t1) - L_lower*cos(sigT15);
+    %% HOMOGENOUS TRANSFORM
+    T16x = Lu*(sin(sigT14) - sin(sigT12)) + ...
+           Ll*(sin(sigT15) - sin( q(1) ));
+    T16y = Lu*(cos(sigT12) - cos(sigT14)) + ...
+           Ll*(cos( q(1) ) - cos(sigT15));
     T16z = -H;
    
     T16 = [cos(sigT), -sin(sigT), 0, T16x;
@@ -39,7 +40,6 @@ function [xe, T16, HTs] = k(q, params)
     phi =   atan2( T16(3,2), T16(3,3) );  
     theta = atan2(-T16(3,1), sqrt( T16(3,2)^2 + T16(3,3)^2 ) );
     psi =   atan2( T16(2,1), T16(1,1) );
-   
     
     xe = [T16(1:3,4); % X Y Z
           phi;        % ϕ
@@ -47,26 +47,26 @@ function [xe, T16, HTs] = k(q, params)
           psi];       % Ψ
     
     %% Homogeneous Transforms
-
-    RZ    = @(psi) ...
-            [cos(psi) -sin(psi)  0  0;
-             sin(psi)  cos(psi)  0  0;
-              0        0         1  0;
-              0        0         0  1];
-    T     = @(x, y, z) ...
-            [eye(3)     [x;y;z];
-             zeros(1,3)      1];
+    T     = @(x, y, z) [    eye(3), [x;y;z] ;
+                        zeros(1,3),    1   ];
 
     HTs.A01 = T(0,0,0);
-        HTs.A12 = RZ(t1)*T(0,L_lower,0);
-        HTs.A23 = RZ(t2)*T(0,L_upper,0);
-        HTs.A34 = RZ(t3)*T(0,0,-H);
-        HTs.A45 = RZ(t4)*T(0,-L_upper,0);
-        HTs.A56 = RZ(t5)*T(0,-L_lower,0);
-    HTs.A02 = HTs.A01*HTs.A12;
-    HTs.A03 = HTs.A02*HTs.A23;
-    HTs.A04 = HTs.A03*HTs.A34;
-    HTs.A05 = HTs.A04*HTs.A45;
-    HTs.A06 = HTs.A05*HTs.A56;
+    HTs.A02 = [cos(q(1)), -sin(q(1)), 0, -Ll*sin(q(1));
+               sin(q(1)),  cos(q(1)), 0,  Ll*cos(q(1));
+                       0,          0, 1,                  0;
+                       0,          0, 0,                  1];
+    HTs.A03 = [cos(sigT12), -sin(sigT12), 0, -Lu*sin(sigT12)-Ll*sin(q(1));
+               sin(sigT12),  cos(sigT12), 0,  Lu*cos(sigT12)+Ll*cos(q(1));
+                         0,            0, 1,                            0;
+                         0,            0, 0,                            1];
+    HTs.A04 = [cos(sigT13), -sin(sigT13), 0, -Lu*sin(sigT12)-Ll*sin(q(1));
+               sin(sigT13),  cos(sigT13), 0,  Lu*cos(sigT12)+Ll*cos(q(1));
+                         0,            0, 1,                           -H;
+                         0,            0, 0,                            1];
+    HTs.A05 = [cos(sigT14), -sin(sigT14), 0,  Lu*(sin(sigT14)-sin(sigT12))-Ll*sin(q(1));
+               sin(sigT14),  cos(sigT14), 0,  Lu*(cos(sigT12)-cos(sigT14))+Ll*cos(q(1));
+                         0,            0, 1,                                         -H;
+                         0,            0, 0,                                          1];
+    HTs.A06 = HTs.A01*T16;
 
 end
--- MatLab/TwoDimensions/main.m
+++ MatLab/TwoDimensions/main.m
@@ -2,6 +2,8 @@ clear all
 close all
 clc
 
+tic % START TIMING
+
 %% NOTES:
 % 2D KINEMATICALLY REDUNDANT
 %
@@ -24,8 +26,8 @@ model.q             = zeros(6,length(model.tspan)); % Making Space
 model.xe            = zeros(6,length(model.tspan)); % Making Space
 params.fibula       = 0.5;
 params.femur        = 0.5;
-params.HipWidth     = 0.3;
-
+params.HipWidth     = 0.25;
+H                   = params.HipWidth;
 % Masses
 params.mass.femur   = 1;    % Thigh Bone
 params.mass.fibula  = 1;    % Paired with `tibia`
@@ -35,62 +37,61 @@ params.mass.pelvis  = 1.5;  % Waist
 
 %% Initial Position & Orientation
 
-model.q0 = [-pi/15; % θ₁
-             2*pi/15; % θ₂
-            -pi/15;     % θ₃
-             pi/15; % θ₄
-            -2*pi/15; % θ₅
+model.q0 = [-pi/10; % θ₁
+           2*pi/10; % θ₂
+            -pi/10;     % θ₃
+             pi/10; % θ₄
+          -2*pi/10; % θ₅
              0];    % θ₆
 
 model.xe0 = [0; % X
              0; % Y
-          -0.3; % Z
+            -H; % Z
              0; % ϕ
              0; % θ
              0];% Ψ
-
 
 %% Trajectory
 D = diag(1:5,-1);   % Special D - Diag Matrix
 
 q0 = [0.00  0.00  0.00;  % qX vX aX
       0.00  0.00  0.00;  % qY vY aY
-     -0.30  0.00  0.00]; % qZ vZ aZ
+     -H     0.00  0.00]; % qZ vZ aZ
 t0 = 0;
 tt0 = t0.^(0:5).';
 T0 = [tt0, D*tt0, D^2*tt0];
 
 q1 = [0.00  0.00  0.00;  % qX vX aX
       0.00  0.00  0.00;  % qY vY aY
-     -0.30  0.00  0.00]; % qZ vZ aZ
+     -H     0.00  0.00]; % qZ vZ aZ
 t1 = 1;
 tt1 = t1.^(0:5).';
 T1 = [tt1, D*tt1, D^2*tt1];
 
 q2 = [0.25  0.00  0.00;  % qX vX aX
       0.25  0.00  0.00;  % qY vY aY
-     -0.30  0.00  0.00]; % qZ vZ aZ
+     -H     0.00  0.00]; % qZ vZ aZ
 t2 = 3;
 tt2 = t2.^(0:5).';
 T2 = [tt2, D*tt2, D^2*tt2];
 
 q3 = [0.50  0.00  0.00;  % qX vX aX
       0.00  0.00  0.00;  % qY vY aY
-     -0.30  0.00  0.00]; % qZ vZ aZ
+     -H     0.00  0.00]; % qZ vZ aZ
 t3 = 4;
 tt3 = t3.^(0:5).';
 T3 = [tt3, D*tt3, D^2*tt3];
 
 q4 = [0.50  0.00  0.00;  % qX vX aX
       0.00  0.00  0.00;  % qY vY aY
-     -0.30  0.00  0.00]; % qZ vZ aZ
+     -H     0.00  0.00]; % qZ vZ aZ
 t4 = 5;
 tt4 = t4.^(0:5).';
 T4 = [tt4, D*tt4, D^2*tt4];
 
 q5 = [0.50  0.00  0.00;  % qX vX aX
       0.00  0.00  0.00;  % qY vY aY
-     -0.30  0.00  0.00]; % qZ vZ aZ
+     -H     0.00  0.00]; % qZ vZ aZ
 t5 = 6;
 tt5 = t5.^(0:5).';
 T5 = [tt5, D*tt5, D^2*tt5];
@@ -102,26 +103,6 @@ tt = model.tspan.^((0:5).');
 Q = C*tt;    % Targeting Postions First ... Worry about Velocities later
 V = C*D*tt;
 A = C*D^2*tt;
-
-% DEBUG
-% figure('Name','Trajectory')
-% for i=1:length(model.tspan)
-%         clf
-%         hold on
-%         plot3([0 2], [0 0], [0 0],'r', 'LineWidth',0.5); % Z
-%         plot3([0 0], [0 2], [0 0],'g', 'LineWidth',0.5); % X
-%         plot3([0 0], [0 0], [0 2],'b', 'LineWidth',0.5); % Y
-%         plot3(Q(3,1:i),Q(1,1:i),Q(2,1:i), 'k-','LineWidth', 0.5);
-%         % Way Points
-%         plot3(q0(3,1),q0(1,1),q0(2,1),'rx','LineWidth',0.5);
-%         plot3(q1(3,1),q1(1,1),q1(2,1),'gx','LineWidth',0.5);
-%         plot3(q2(3,1),q2(1,1),q2(2,1),'bx','LineWidth',0.5);
-%         plot3(q3(3,1),q3(1,1),q3(2,1),'cx','LineWidth',0.5);
-%         legend('+Z','+X','+Y','Trajectory','WP0','WP1','WP2','WP3','Autoupdate','off');
-%         %view(135,35);
-%         view(90,0);
-%         pause(0.01)
-% end
 
 %% LOOP
 
@@ -135,13 +116,14 @@ for i=2:length(model.tspan)
     model.q(:,i) = k_Inv(model.q(:,i-1), model.xe(:,i), params);
 end
 
+toc % FINISH TIMING
 
 %% Graph
 figure('Name','Animation')
 
 for i=1:length(model.tspan)
     
-    [~, ~, HomegeneousTransforms] = k(model.q(:,i), params);
+    [~, T16, HomegeneousTransforms] = k(model.q(:,i), params);
     r0CoM = rCoM(model.q(:,i),params);
 
     % DEBUG
@@ -185,7 +167,7 @@ for i=1:length(model.tspan)
     'k', 'LineWidth',2);
     plot3(r05(3), r05(1), r05(2), 'bx', 'LineWidth',2);         % JOINT FIVE
     % SIX 
-    r06 = HomegeneousTransforms.A06(1:3,4);
+    r06 = T16(1:3,4);
     plot3([r05(3) r06(3)], [r05(1) r06(1)], [r05(2) r06(2)],... % LINK
     'k', 'LineWidth',2);
     plot3(r06(3), r06(1), r06(2), 'bx', 'LineWidth',2);         % JOINT SIX
@@ -198,21 +180,16 @@ for i=1:length(model.tspan)
     axis([-1 1 -0.5 2 0 2]);
     view(135,35);
     %view(90,0); % -> 2D
-    FRAME(i) = getframe(gcf);
+    IMAGE(i) = getframe(gcf);
     drawnow
 end
 
-% create the video writer with 1 fps
-writerObj = VideoWriter('2D_Step.mp4','MPEG-4');
-writerObj.FrameRate = params.framerate;
-% set the seconds per image
-% open the video writer
-open(writerObj);                        
-% write the frames to the video
-for i=1:length(FRAME)
-    % convert the image to a frame
-    frame = FRAME(i) ;    
-    writeVideo(writerObj, frame);
+%% VIDEO
+videoWriterObj           = VideoWriter('2D_Step.mp4','MPEG-4');
+videoWriterObj.FrameRate = params.framerate;
+open(videoWriterObj);                        
+for i=1:length(IMAGE)
+    frame = IMAGE(i);   % Convert from an Image to a Frame
+    writeVideo(videoWriterObj, frame);
 end
-% close the writer object
-close(writerObj);
+close(videoWriterObj);
