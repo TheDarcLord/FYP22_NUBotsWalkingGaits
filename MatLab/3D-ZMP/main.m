9359d22e23b2c39327bfab709c81b32c6b1edc3f
Darcy Byrne <darcybyrne3@gmail.com>
2022-05-30 02:19:17 +1000

    SAVE THE WORKSPACE

--- MatLab/3D-ZMP/main.m
+++ MatLab/3D-ZMP/main.m
@@ -27,15 +27,15 @@ tic % START TIMING
  % Physical Parameters - Affect CoM or FKM
     params.kx        = 0;        % These affect the plane to which    |
     params.ky        = 0;        % ... the CoM is constrained         |
-    params.zc        = 0.3148;   % m    - Height of the CoM ^         |
+    params.zc        = 0.4564;   % m    - Height of the CoM ^         |
     params.g         = 9.81;     % ms⁻² - Acceleration due to Gravity |
     params.m         = 7.4248;   % kg   - Total Mass of a NuGus       |
  % -------------------------------------------------------------------|
-    params.fibula    = 0.22;     % m    - Lower leg
-    params.femur     = 0.22;     % m    - Upper Leg
-    params.HipWidth  = 0.15;     % m    - Pelvis
+    params.fibula    = 0.4;     % m    - Lower leg
+    params.femur     = 0.4;     % m    - Upper Leg
+    params.HipWidth  = 0.2;     % m    - Pelvis
     params.ServoSize = 0.05;     % m    - Approximation/Spacing
-    params.StepSize  = 0.1;      % m    - 10 cm Step forward
+    params.StepSize  = 0.2;      % m    - 10 cm Step forward
  % Masses
     params.mass.fibula = 1.5;    % Paired with `tibia`
     params.mass.femur  = 1.5;    % Thigh Bone
@@ -63,16 +63,16 @@ tic % START TIMING
 
 %% Initial Position & Orientation
 model.r.q0 = [      0;     % θ₁    
-                -pi/24;     % θ₂    ->  2D θ₁ Ankle
-               2*pi/24;     % θ₃    ->  2D θ₂ Knee
-                -pi/24;     % θ₄    ->  2D θ₃ Hip
+                -pi/6;     % θ₂    ->  2D θ₁ Ankle
+               2*pi/6;     % θ₃    ->  2D θ₂ Knee
+                -pi/6;     % θ₄    ->  2D θ₃ Hip
                     0;     % θ₅
                     0;     % θ₆
                     0;     % θ₇
                     0;     % θ₈
-                 pi/24;     % θ₉    ->  2D θ₄ Hip
-              -2*pi/24;     % θ₁₀   ->  2D θ₅ Knee
-                 pi/24;     % θ₁₁   ->  2D θ₆ Ankle
+                 pi/6;     % θ₉    ->  2D θ₄ Hip
+              -2*pi/6;     % θ₁₀   ->  2D θ₅ Knee
+                 pi/6;     % θ₁₁   ->  2D θ₆ Ankle
                     0];    % θ₁₂
 
     model.r.q(:,1)            = model.r.q0;
@@ -203,18 +203,20 @@ toc % FINISH TIMING
 
 %% Animation Pendulum
 ANIMATION_PEND = figure(42);
-    hold on
-    grid on
+   
     for i=1:length(model.tspan)
-        cla(ANIMATION_PEND)
+        Pend = subplot(1,2,1);
+        hold on
+        grid on
+        cla(Pend)    % Clears Data but not Titles/Labels  
         
         title("Pendulum " + model.tspan(i))
         view(135,35);
-        axis([-0.15 0.15 -0.1 0.5 0 0.75])
+        axis([-0.4,0.2, -1,1, 0,1]);
         % ZERO:  Z      X      Y
-        plot3([0 1], [0 0], [0 0],'r', 'LineWidth',0.5); % Z
-        plot3([0 0], [0 1], [0 0],'g', 'LineWidth',0.5); % X
-        plot3([0 0], [0 0], [0 1],'b', 'LineWidth',0.5); % Y
+        plot3([0 1], [0 0], [0 0],'r', 'LineWidth',2); % Z
+        plot3([0 0], [0 1], [0 0],'LineWidth',2,'Color','#379203'); % X
+        plot3([0 0], [0 0], [0 1],'b', 'LineWidth',2); % Y
         if i < 2
             legend('+Z','+X','+Y', 'Trajectory','Autoupdate','off');
             xlabel('Z','FontWeight','bold');
@@ -224,157 +226,152 @@ ANIMATION_PEND = figure(42);
         plot3(model.p.pREF(2,i),... % Y|Z
               model.p.pREF(1,i),... % X
               0,...
-              'kx','LineWidth',2,'MarkerSize',5)
+              'kx','LineWidth',3,'MarkerSize',10)
         plot3(model.p.x(4,i),...    % Y|Z
               model.p.x(1,i),...    % X
               params.zc,...
-              'ro','LineWidth',1,'MarkerSize',5)
+              'ro','LineWidth',3,'MarkerSize',10)
         plot3(model.p.x(4,1:i),...  % Y|Z
               model.p.x(1,1:i),...  % X
               params.zc*ones(1,length(model.tspan(1:i))),...
-              'r','LineWidth',1)
+              'r','LineWidth',2)
+        drawnow
+    %end
+
+% Animation Biped
+
+%for i=1:length(model.tspan)
+        Robot = subplot(1,2,2);
+        cla(Robot)    % Clears Data but not Titles/Labels  
+        hold on
+        grid on
+    
+%         txt = " Time: " + num2str(model.tspan(i)) + " sec";
+%         text(0,0.5,0.5,txt)
+        
+        if i > 401
+            params.mode     = -1;
+        elseif i > 201
+            params.mode     =  1;
+        else 
+            params.mode     = -1;
+        end
+    
+        set(gca,'Color','#CCCCCC');
+        [~, HTs]    = k( model.r.q(:,i),i,model,params);
+    
+        % ZERO:  Z      X      Y
+        plot3([0 1], [0 0], [0 0],'r', 'LineWidth',2); % Z
+        plot3([0 0], [0 1], [0 0],'LineWidth',2,'Color','#379203'); % X
+        plot3([0 0], [0 0], [0 1],'b', 'LineWidth',2); % Y
+        if i < 2
+            legend('+Z','+X','+Y', 'Trajectory','Autoupdate','off');
+            title("Stand | Initial Step | Multiple Steps")
+            xlabel('Z','FontWeight','bold');
+            ylabel('X','FontWeight','bold');
+            zlabel('Y','FontWeight','bold');
+        end
+        % MAIN COMPONENTS
+        % Z X Y
+    
+        % END EFFECTOR RIGHT!
+        r0ER = HTs.A0ER(1:3,4);
+        plot3(r0ER(3), r0ER(1), r0ER(2), 'bo', 'LineWidth',0.5,'MarkerSize',10);      % J End E
+        % END EFFECTOR LEFT!
+        r0EL = HTs.A0EL(1:3,4);
+        plot3(r0EL(3), r0EL(1), r0EL(2), 'ro', 'LineWidth',0.5,'MarkerSize',10);      % J End E
+    
+        % ONE
+        r01 = HTs.A01(1:3,4);
+        plot3(r01(3), r01(1), r01(2), 'rx', 'LineWidth',2);         % J ONE
+        % TWO
+        r02 = HTs.A02(1:3,4);
+        plot3([r01(3) r02(3)], [r01(1) r02(1)], [r01(2) r02(2)],... % L TWO
+        'k', 'LineWidth',2);
+        plot3(r02(3), r02(1), r02(2) ,'rx', 'LineWidth',2);         % J TWO
+        % THREE
+        r03 = HTs.A03(1:3,4);
+        plot3([r02(3) r03(3)], [r02(1) r03(1)], [r02(2) r03(2)],... % L THREE
+        'k', 'LineWidth',2);
+        plot3(r03(3), r03(1), r03(2), 'rx', 'LineWidth',2);         % J THREE
+        % FOUR
+        r04 = HTs.A04(1:3,4);
+        plot3([r03(3) r04(3)], [r03(1) r04(1)], [r03(2) r04(2)],... % L FOUR
+        'k', 'LineWidth',2);
+        plot3(r04(3), r04(1), r04(2), 'rx', 'LineWidth',2);         % J FOUR
+        % FIVE
+        r05 = HTs.A05(1:3,4);
+        plot3([r04(3) r05(3)], [r04(1) r05(1)], [r04(2) r05(2)],... % L FIVE
+        'k', 'LineWidth',2);
+        plot3(r05(3), r05(1), r05(2), 'rx', 'LineWidth',2);         % J FIVE
+        % SIX 
+        r06 = HTs.A06(1:3,4);
+        plot3([r05(3) r06(3)], [r05(1) r06(1)], [r05(2) r06(2)],... % L SIX
+        'k', 'LineWidth',2);
+        plot3(r06(3), r06(1), r06(2), 'rx', 'LineWidth',2);         % J SIX
+
+        % SEVEN 
+        r07 = HTs.A07(1:3,4);
+        plot3([r06(3) r07(3)], [r06(1) r07(1)], [r06(2) r07(2)],... % L SEVEN
+        'k', 'LineWidth',2);
+        plot3(r07(3), r07(1), r07(2), 'bx', 'LineWidth',2);         % J SEVEN
+        % EIGHT
+        r08 = HTs.A08(1:3,4);
+        plot3([r07(3) r08(3)], [r07(1) r08(1)], [r07(2) r08(2)],... % L EIGHT
+        'k', 'LineWidth',2);
+        plot3(r08(3), r08(1), r08(2), 'bx', 'LineWidth',2);         % J EIGHT
+        % NINE
+        r09 = HTs.A09(1:3,4);
+        plot3([r08(3) r09(3)], [r08(1) r09(1)], [r08(2) r09(2)],... % L NINE
+        'k', 'LineWidth',2);
+        plot3(r09(3), r09(1), r09(2), 'bx', 'LineWidth',2);         % J NINE
+        % TEN
+        r010= HTs.A010(1:3,4);
+        plot3([r09(3) r010(3)], [r09(1) r010(1)], [r09(2) r010(2)],... % L TEN
+        'k', 'LineWidth',2);
+        plot3(r010(3), r010(1), r010(2), 'bx', 'LineWidth',2);         % J TEN
+        % ELEVEN
+        r011 = HTs.A011(1:3,4);
+        plot3([r010(3) r011(3)], [r010(1) r011(1)], [r010(2) r011(2)],... % L ELEVEN
+        'k', 'LineWidth',2);
+        plot3(r011(3), r011(1), r011(2), 'bx', 'LineWidth',2);            % J ELEVEN
+        % TWELEVE
+        r012 = HTs.A0ER(1:3,4);
+        plot3([r011(3) r012(3)], [r011(1) r012(1)], [r011(2) r012(2)],... % L TWELEVE
+        'k', 'LineWidth',2);
+        plot3(r012(3), r012(1), r012(2), 'bx', 'LineWidth',2);            % J TWELEVE
+     
+        
+        plot3(model.p.pREF(2,i),... % Y|Z
+                  model.p.pREF(1,i),... % X
+                  0,...
+                  'kx','LineWidth',2,'MarkerSize',5)
+        plot3([model.p.pREF(2,i), model.p.x(4,i)],... % Y|Z
+              [model.p.pREF(1,i), model.p.x(1,i)],... % X
+              [0, params.zc],...
+              'm:','LineWidth',2)
+        plot3(model.p.x(4,i),...    % Y|Z
+              model.p.x(1,i),...    % X
+              params.zc,...
+              'mx','LineWidth',1,'MarkerSize',5)
+        plot3(model.p.x(4,1:i),...  % Y|Z
+              model.p.x(1,1:i),...  % X
+              params.zc*ones(1,length(model.tspan(1:i))),...
+              'm','LineWidth',1)
+    
+        plot3(model.r.r0CoMg(3,i),model.r.r0CoMg(1,i),model.r.r0CoMg(2,i),'mo', 'LineWidth',1.5);
+    
+        %    [         MIN,          MAX, ...
+        axis([          -0.5,        0.3, ...
+              model.r.r0CoMg(3,i)-1, model.r.r0CoMg(1,i)+1, ...
+                         0,          1]);
+        view(135,35);
+        % view(90,0); % -> 2D
+        IMAGE(i) = getframe(gcf);
         drawnow
     end
 
-%% Animation Biped
-ANIMATION = figure(69);
-
-for i=1:length(model.tspan)
-
-    cla(ANIMATION)    % Clears Data but not Titles/Labels  
-    hold on
-    grid on
-
-    txt = " Time: " + num2str(model.tspan(i)) + " sec";
-    text(0,0.5,0.5,txt)
-    
-    if i > 401
-        params.mode     = -1;
-    elseif i > 201
-        params.mode     =  1;
-    else 
-        params.mode     = -1;
-    end
-
-    set(gca,'Color','#CCCCCC');
-    [~, HTs]    = k( model.r.q(:,i),i,model,params);
-
-    % ZERO:  Z      X      Y
-    plot3([0 1], [0 0], [0 0],'r', 'LineWidth',0.5); % Z
-    plot3([0 0], [0 1], [0 0],'g', 'LineWidth',0.5); % X
-    plot3([0 0], [0 0], [0 1],'b', 'LineWidth',0.5); % Y
-    if i < 2
-        legend('+Z','+X','+Y', 'Trajectory','Autoupdate','off');
-        title("Stand | Initial Step | Multiple Steps")
-        xlabel('Z','FontWeight','bold');
-        ylabel('X','FontWeight','bold');
-        zlabel('Y','FontWeight','bold');
-    end
-    % MAIN COMPONENTS
-    % Z X Y
-
-    % END EFFECTOR RIGHT!
-    r0ER = HTs.A0ER(1:3,4);
-    plot3(r0ER(3), r0ER(1), r0ER(2), 'bo', 'LineWidth',0.5,'MarkerSize',10);      % J End E
-    % END EFFECTOR LEFT!
-    r0EL = HTs.A0EL(1:3,4);
-    plot3(r0EL(3), r0EL(1), r0EL(2), 'co', 'LineWidth',0.5,'MarkerSize',10);      % J End E
-
-    % ONE
-    r01 = HTs.A01(1:3,4);
-    plot3(r01(3), r01(1), r01(2), 'cx', 'LineWidth',2);         % J ONE
-    % TWO
-    r02 = HTs.A02(1:3,4);
-    plot3([r01(3) r02(3)], [r01(1) r02(1)], [r01(2) r02(2)],... % L TWO
-    'k', 'LineWidth',2);
-    plot3(r02(3), r02(1), r02(2) ,'cx', 'LineWidth',2);         % J TWO
-    % THREE
-    r03 = HTs.A03(1:3,4);
-    plot3([r02(3) r03(3)], [r02(1) r03(1)], [r02(2) r03(2)],... % L THREE
-    'k', 'LineWidth',2);
-    plot3(r03(3), r03(1), r03(2), 'cx', 'LineWidth',2);         % J THREE
-    % FOUR
-    r04 = HTs.A04(1:3,4);
-    plot3([r03(3) r04(3)], [r03(1) r04(1)], [r03(2) r04(2)],... % L FOUR
-    'k', 'LineWidth',2);
-    plot3(r04(3), r04(1), r04(2), 'cx', 'LineWidth',2);         % J FOUR
-    % FIVE
-    r05 = HTs.A05(1:3,4);
-    plot3([r04(3) r05(3)], [r04(1) r05(1)], [r04(2) r05(2)],... % L FIVE
-    'k', 'LineWidth',2);
-    plot3(r05(3), r05(1), r05(2), 'cx', 'LineWidth',2);         % J FIVE
-    % SIX 
-    r06 = HTs.A06(1:3,4);
-    plot3([r05(3) r06(3)], [r05(1) r06(1)], [r05(2) r06(2)],... % L SIX
-    'k', 'LineWidth',2);
-    plot3(r06(3), r06(1), r06(2), 'cx', 'LineWidth',2);         % J SIX
-
-    % MID WAIST
-    r0H = HTs.A0H(1:3,4);
-    plot3(r0H(3), r0H(1), r0H(2), 'mx', 'LineWidth',2);         % J MID WST
-
-    % SEVEN 
-    r07 = HTs.A07(1:3,4);
-    plot3([r06(3) r07(3)], [r06(1) r07(1)], [r06(2) r07(2)],... % L SEVEN
-    'k', 'LineWidth',2);
-    plot3(r07(3), r07(1), r07(2), 'bx', 'LineWidth',2);         % J SEVEN
-    % EIGHT
-    r08 = HTs.A08(1:3,4);
-    plot3([r07(3) r08(3)], [r07(1) r08(1)], [r07(2) r08(2)],... % L EIGHT
-    'k', 'LineWidth',2);
-    plot3(r08(3), r08(1), r08(2), 'bx', 'LineWidth',2);         % J EIGHT
-    % NINE
-    r09 = HTs.A09(1:3,4);
-    plot3([r08(3) r09(3)], [r08(1) r09(1)], [r08(2) r09(2)],... % L NINE
-    'k', 'LineWidth',2);
-    plot3(r09(3), r09(1), r09(2), 'bx', 'LineWidth',2);         % J NINE
-    % TEN
-    r010= HTs.A010(1:3,4);
-    plot3([r09(3) r010(3)], [r09(1) r010(1)], [r09(2) r010(2)],... % L TEN
-    'k', 'LineWidth',2);
-    plot3(r010(3), r010(1), r010(2), 'bx', 'LineWidth',2);         % J TEN
-    % ELEVEN
-    r011 = HTs.A011(1:3,4);
-    plot3([r010(3) r011(3)], [r010(1) r011(1)], [r010(2) r011(2)],... % L ELEVEN
-    'k', 'LineWidth',2);
-    plot3(r011(3), r011(1), r011(2), 'bx', 'LineWidth',2);            % J ELEVEN
-    % TWELEVE
-    r012 = HTs.A0ER(1:3,4);
-    plot3([r011(3) r012(3)], [r011(1) r012(1)], [r011(2) r012(2)],... % L TWELEVE
-    'k', 'LineWidth',2);
-    plot3(r012(3), r012(1), r012(2), 'bx', 'LineWidth',2);            % J TWELEVE
- 
-    
-    plot3(model.p.pREF(2,i),... % Y|Z
-              model.p.pREF(1,i),... % X
-              0,...
-              'kx','LineWidth',2,'MarkerSize',5)
-    plot3([model.p.pREF(2,i), model.p.x(4,i)],... % Y|Z
-          [model.p.pREF(1,i), model.p.x(1,i)],... % X
-          [0, params.zc],...
-          'r-','LineWidth',2)
-    plot3(model.p.x(4,i),...    % Y|Z
-          model.p.x(1,i),...    % X
-          params.zc,...
-          'rx','LineWidth',1,'MarkerSize',5)
-    plot3(model.p.x(4,1:i),...  % Y|Z
-          model.p.x(1,1:i),...  % X
-          params.zc*ones(1,length(model.tspan(1:i))),...
-          'r','LineWidth',1)
-
-    plot3(model.r.r0CoMg(3,i),model.r.r0CoMg(1,i),model.r.r0CoMg(2,i),'ro', 'LineWidth',1.5);
-
-    %    [         MIN,          MAX, ...
-    axis([          -0.45,        0.30, ...
-          model.r.r0CoMg(3,i)-0.5, model.r.r0CoMg(1,i)+0.5, ...
-                     0,          1]);
-    view(135,35);
-    % view(90,0); % -> 2D
-    IMAGE(i) = getframe(gcf);
-    drawnow
-end
-
-%% VIDEO
+% VIDEO
 videoWriterObj           = VideoWriter('3D_Step.mp4','MPEG-4');
 videoWriterObj.FrameRate = params.framerate; % 15sec video
 open(videoWriterObj);                        
