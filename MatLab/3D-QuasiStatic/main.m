7e8e048b2ab29db09dd474fad589f2649c3a5eb8
Darcy Byrne <darcybyrne3@gmail.com>
2022-05-29 21:17:20 +1000

    Create Initial matlab figure of 3D model

--- MatLab/3D-QuasiStatic/main.m
+++ MatLab/3D-QuasiStatic/main.m
@@ -19,10 +19,10 @@ model.r0CoMg        = zeros(3,length(model.tspan));  % r0CoMg  [XYZ]ᵀ
 params.fibula       = 0.4;
 params.femur        = 0.4;
 params.HipWidth     = 0.2;
-params.ServoSize    = 0.01;
+params.ServoSize    = 0.05;
 
 params.StepSize     = 0.4;
-params.r0Lg         = zeros(3,1);  % Right Position from 0rigin in Global
+params.r0Lg         = [0.2; 0; -0.1];  % Right Position from 0rigin in Global
 params.r0Hg         = zeros(3,1);  % Waist Position from 0rigin in Global
 params.r0Rg         = zeros(3,1);  % Left  Position from 0rigin in Global
 params.r0CoMg       = zeros(3,1);  % CoM   Position from 0rigin in Global
@@ -49,6 +49,127 @@ model.q0 = [    0;     % θ₁
           -2*pi/6;     % θ₁₀   ->  2D θ₅ Knee
              pi/6;     % θ₁₁   ->  2D θ₆ Ankle
                 0];    % θ₁₂ 
+
+model.q(:,1)            = model.q0;
+[model.xe(:,1), HTs]    = k(model.q0, params);
+
+model.r0Rg(:,1)         = HTs.A0ER(1:3,4);
+    params.r0Rg         = model.r0Rg(:,1);
+model.r0Lg(:,1)         = HTs.A0EL(1:3,4);
+    params.r0Lg         = model.r0Lg(:,1);
+model.r0Hg(:,1)         = HTs.A0H(1:3,4);
+    params.r0Hg         = model.r0Hg(:,1);
+model.r0CoMg(:,1)       = rCoM(model.q(:,1),params);
+    params.r0CoMg       = model.r0CoMg(:,1);
+
+%% Initial Figure
+i = 1;
+InitialFigure = figure(1);
+    cla    % Clears Data but not Titles/Labels  
+    hold on
+    grid on
+    set(gca,'Color','#CCCCCC');
+    view(145,20);
+    
+    params.r0Lg     = model.r0Lg(:,i);
+    params.r0Rg     = model.r0Rg(:,i);
+    params.r0Hg     = model.r0Hg(:,i);
+    params.r0CoMg   = model.r0CoMg(:,i);
+
+    set(gca,'Color','#CCCCCC');
+    [~, HTs]    = k(model.q(:,i), params);
+
+    % ZERO:  Z      X      Y
+    plot3([0 1], [0 0], [0 0],'r', 'LineWidth',3); % Z
+    plot3([0 0], [0 1], [0 0], 'LineWidth',3,'Color','#379203'); % X
+    plot3([0 0], [0 0], [0 1],'b', 'LineWidth',3); % Y
+    quiver3(0,0,0,model.r0Lg(3,1),model.r0Lg(1,1),0,...
+        'LineWidth',2,'Color','k','ShowArrowHead','on',...
+        'LineStyle',':','AutoScale','off')
+    title("3D Model",'FontSize',18);
+    xlabel('{\bfZ} (metres)');
+    ylabel('{\bfX} (metres)');
+    zlabel('{\bfY} (metres)');
+    % MAIN COMPONENTS
+
+    % END EFFECTOR RIGHT!
+    r0ER = HTs.A0ER(1:3,4);
+    plot3(r0ER(3), r0ER(1), r0ER(2), 'bo', 'LineWidth',0.5,'MarkerSize',10);      % J End E
+    % END EFFECTOR LEFT!
+    r0EL = HTs.A0EL(1:3,4);
+    plot3(r0EL(3), r0EL(1), r0EL(2), 'ro', 'LineWidth',0.5,'MarkerSize',10);      % J End E
+
+    % ONE
+    r01 = HTs.A01(1:3,4);
+    plot3(r01(3), r01(1), r01(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J ONE
+    % TWO
+    r02 = HTs.A02(1:3,4);
+    plot3([r01(3) r02(3)], [r01(1) r02(1)], [r01(2) r02(2)],... % L TWO
+    'k', 'LineWidth',2);
+    plot3(r02(3), r02(1), r02(2) ,'rx', 'LineWidth',3,'MarkerSize',10);         % J TWO
+    % THREE
+    r03 = HTs.A03(1:3,4);
+    plot3([r02(3) r03(3)], [r02(1) r03(1)], [r02(2) r03(2)],... % L THREE
+    'k', 'LineWidth',2);
+    plot3(r03(3), r03(1), r03(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J THREE
+    % FOUR
+    r04 = HTs.A04(1:3,4);
+    plot3([r03(3) r04(3)], [r03(1) r04(1)], [r03(2) r04(2)],... % L FOUR
+    'k', 'LineWidth',2);
+    plot3(r04(3), r04(1), r04(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J FOUR
+    % FIVE
+    r05 = HTs.A05(1:3,4);
+    plot3([r04(3) r05(3)], [r04(1) r05(1)], [r04(2) r05(2)],... % L FIVE
+    'k', 'LineWidth',2);
+    plot3(r05(3), r05(1), r05(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J FIVE
+    % SIX 
+    r06 = HTs.A06(1:3,4);
+    plot3([r05(3) r06(3)], [r05(1) r06(1)], [r05(2) r06(2)],... % L SIX
+    'k', 'LineWidth',2);
+    plot3(r06(3), r06(1), r06(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J SIX
+
+    % MID WAIST
+    r0H = HTs.A0H(1:3,4);
+    plot3(r0H(3), r0H(1), r0H(2), 'mx', 'LineWidth',3,'MarkerSize',10);         % J MID WST
+
+    % SEVEN 
+    r07 = HTs.A07(1:3,4);
+    plot3([r06(3) r07(3)], [r06(1) r07(1)], [r06(2) r07(2)],... % L SEVEN
+    'k', 'LineWidth',2);
+    plot3(r07(3), r07(1), r07(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J SEVEN
+    % EIGHT
+    r08 = HTs.A08(1:3,4);
+    plot3([r07(3) r08(3)], [r07(1) r08(1)], [r07(2) r08(2)],... % L EIGHT
+    'k', 'LineWidth',2);
+    plot3(r08(3), r08(1), r08(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J EIGHT
+    % NINE
+    r09 = HTs.A09(1:3,4);
+    plot3([r08(3) r09(3)], [r08(1) r09(1)], [r08(2) r09(2)],... % L NINE
+    'k', 'LineWidth',2);
+    plot3(r09(3), r09(1), r09(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J NINE
+    % TEN
+    r010= HTs.A010(1:3,4);
+    plot3([r09(3) r010(3)], [r09(1) r010(1)], [r09(2) r010(2)],... % L TEN
+    'k', 'LineWidth',2);
+    plot3(r010(3), r010(1), r010(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J TEN
+    % ELEVEN
+    r011 = HTs.A011(1:3,4);
+    plot3([r010(3) r011(3)], [r010(1) r011(1)], [r010(2) r011(2)],... % L ELEVEN
+    'k', 'LineWidth',2);
+    plot3(r011(3), r011(1), r011(2), 'bx', 'LineWidth',3,'MarkerSize',10);            % J ELEVEN
+    % TWELEVE
+    r012 = HTs.A0ER(1:3,4);
+    plot3([r011(3) r012(3)], [r011(1) r012(1)], [r011(2) r012(2)],... % L TWELEVE
+    'k', 'LineWidth',2);
+    plot3(r012(3), r012(1), r012(2), 'bx', 'LineWidth',3,'MarkerSize',10);            % J TWELEVE
+
+    legend({'+Z_0','+X_0','+Y_0','{r}^0_{E_{Left}} - \it{Link 1}',...
+            '','','Joints 1 - 3','','','','','','','','','','',...
+            'Mid Waist','','Joints 4 - 6'},...
+            'FontSize',16,Location='west');
+    
+    axis([-0.4,0.2, -1,1, 0,1]);
+
 
 %% LOOP
 % Initial Conditions
@@ -261,67 +382,67 @@ for i=1:length(model.tspan)
 
     % ONE
     r01 = HTs.A01(1:3,4);
-    plot3(r01(3), r01(1), r01(2), 'cx', 'LineWidth',2);         % J ONE
+    plot3(r01(3), r01(1), r01(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J ONE
     % TWO
     r02 = HTs.A02(1:3,4);
     plot3([r01(3) r02(3)], [r01(1) r02(1)], [r01(2) r02(2)],... % L TWO
     'k', 'LineWidth',2);
-    plot3(r02(3), r02(1), r02(2) ,'cx', 'LineWidth',2);         % J TWO
+    plot3(r02(3), r02(1), r02(2) ,'rx', 'LineWidth',3,'MarkerSize',10);         % J TWO
     % THREE
     r03 = HTs.A03(1:3,4);
     plot3([r02(3) r03(3)], [r02(1) r03(1)], [r02(2) r03(2)],... % L THREE
     'k', 'LineWidth',2);
-    plot3(r03(3), r03(1), r03(2), 'cx', 'LineWidth',2);         % J THREE
+    plot3(r03(3), r03(1), r03(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J THREE
     % FOUR
     r04 = HTs.A04(1:3,4);
     plot3([r03(3) r04(3)], [r03(1) r04(1)], [r03(2) r04(2)],... % L FOUR
     'k', 'LineWidth',2);
-    plot3(r04(3), r04(1), r04(2), 'cx', 'LineWidth',2);         % J FOUR
+    plot3(r04(3), r04(1), r04(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J FOUR
     % FIVE
     r05 = HTs.A05(1:3,4);
     plot3([r04(3) r05(3)], [r04(1) r05(1)], [r04(2) r05(2)],... % L FIVE
     'k', 'LineWidth',2);
-    plot3(r05(3), r05(1), r05(2), 'cx', 'LineWidth',2);         % J FIVE
+    plot3(r05(3), r05(1), r05(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J FIVE
     % SIX 
     r06 = HTs.A06(1:3,4);
     plot3([r05(3) r06(3)], [r05(1) r06(1)], [r05(2) r06(2)],... % L SIX
     'k', 'LineWidth',2);
-    plot3(r06(3), r06(1), r06(2), 'cx', 'LineWidth',2);         % J SIX
+    plot3(r06(3), r06(1), r06(2), 'rx', 'LineWidth',3,'MarkerSize',10);         % J SIX
 
     % MID WAIST
     r0H = HTs.A0H(1:3,4);
-    plot3(r0H(3), r0H(1), r0H(2), 'mx', 'LineWidth',2);         % J MID WST
+    plot3(r0H(3), r0H(1), r0H(2), 'mx', 'LineWidth',3,'MarkerSize',10);         % J MID WST
 
     % SEVEN 
     r07 = HTs.A07(1:3,4);
     plot3([r06(3) r07(3)], [r06(1) r07(1)], [r06(2) r07(2)],... % L SEVEN
     'k', 'LineWidth',2);
-    plot3(r07(3), r07(1), r07(2), 'bx', 'LineWidth',2);         % J SEVEN
+    plot3(r07(3), r07(1), r07(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J SEVEN
     % EIGHT
     r08 = HTs.A08(1:3,4);
     plot3([r07(3) r08(3)], [r07(1) r08(1)], [r07(2) r08(2)],... % L EIGHT
     'k', 'LineWidth',2);
-    plot3(r08(3), r08(1), r08(2), 'bx', 'LineWidth',2);         % J EIGHT
+    plot3(r08(3), r08(1), r08(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J EIGHT
     % NINE
     r09 = HTs.A09(1:3,4);
     plot3([r08(3) r09(3)], [r08(1) r09(1)], [r08(2) r09(2)],... % L NINE
     'k', 'LineWidth',2);
-    plot3(r09(3), r09(1), r09(2), 'bx', 'LineWidth',2);         % J NINE
+    plot3(r09(3), r09(1), r09(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J NINE
     % TEN
     r010= HTs.A010(1:3,4);
     plot3([r09(3) r010(3)], [r09(1) r010(1)], [r09(2) r010(2)],... % L TEN
     'k', 'LineWidth',2);
-    plot3(r010(3), r010(1), r010(2), 'bx', 'LineWidth',2);         % J TEN
+    plot3(r010(3), r010(1), r010(2), 'bx', 'LineWidth',3,'MarkerSize',10);         % J TEN
     % ELEVEN
     r011 = HTs.A011(1:3,4);
     plot3([r010(3) r011(3)], [r010(1) r011(1)], [r010(2) r011(2)],... % L ELEVEN
     'k', 'LineWidth',2);
-    plot3(r011(3), r011(1), r011(2), 'bx', 'LineWidth',2);            % J ELEVEN
+    plot3(r011(3), r011(1), r011(2), 'bx', 'LineWidth',3,'MarkerSize',10);            % J ELEVEN
     % TWELEVE
     r012 = HTs.A0ER(1:3,4);
     plot3([r011(3) r012(3)], [r011(1) r012(1)], [r011(2) r012(2)],... % L TWELEVE
     'k', 'LineWidth',2);
-    plot3(r012(3), r012(1), r012(2), 'bx', 'LineWidth',2);            % J TWELEVE
+    plot3(r012(3), r012(1), r012(2), 'bx', 'LineWidth',3,'MarkerSize',10);            % J TWELEVE
 
     plot3(params.r0CoMg(3),params.r0CoMg(1),params.r0CoMg(2), 'ro', 'LineWidth',1.5);
 
