f548c8a7279bd499950664fb83e82b6dd65a5f13
Darcy Byrne <darcybyrne3@gmail.com>
2022-08-03 18:54:37 +1000

    Update kSLOW.m

--- MatLab/3D-ZMP/kSLOW.m
+++ MatLab/3D-ZMP/kSLOW.m
@@ -57,359 +57,108 @@ function [HTs] = kSLOW(q, index, model, params)
     q12 = q(12);    % θ₁₂
 
     %% HOMOGENOUS TRANSFORM
-    if params.mode ==  1        % LEFT FIXED
-        HTs.A0EL = A0EL;
-        HTs.A01  = HTs.A0EL * AE1;
+    HTs.A0EL = A0EL;
+    HTs.A01 = HTs.A0EL * AE1;
 
-        A12x = -S*sin(q1);
-        A12y =  S*cos(q1);
-        A12z =  0;
-        A12R = [0, -sin(q1), -cos(q1);
-                0,  cos(q1), -sin(q1);
-                1,        0,        0];
-        A12  = [A12R, [A12x A12y A12z]'; ROW4];
-        HTs.A02 = HTs.A01 * A12;
+    A12x = -S*sin(q1);
+    A12y =  S*cos(q1);
+    A12z =  0;
+    A12R = [0, -sin(q1), -cos(q1);
+            0,  cos(q1), -sin(q1);
+            1,        0,        0];
+    A12  = [A12R, [A12x A12y A12z]'; ROW4];
+    HTs.A02 = HTs.A01 * A12;
 
-        A23x = -Ll*sin(q2);
-        A23y =  Ll*cos(q2);
-        A23z =  0;
-        A23R = [cos(q2), -sin(q2), 0;
-                sin(q2),  cos(q2), 0;
-                      0,        0, 1];
-        A23  = [A23R, [A23x A23y A23z]'; ROW4];
-        HTs.A03 = HTs.A02 * A23;
+    A23x = -Ll*sin(q2);
+    A23y =  Ll*cos(q2);
+    A23z =  0;
+    A23R = [cos(q2), -sin(q2), 0;
+            sin(q2),  cos(q2), 0;
+                  0,        0, 1];
+    A23  = [A23R, [A23x A23y A23z]'; ROW4];
+    HTs.A03 = HTs.A02 * A23;
 
-        A34x = -Lu*sin(q3);
-        A34y =  Lu*cos(q3);
-        A34z =  0;
-        A34R = [cos(q3), -sin(q3), 0;
-                sin(q3),  cos(q3), 0;
-                      0,        0, 1];
-        A34  = [A34R, [A34x A34y A34z]'; ROW4];
-        HTs.A04 = HTs.A03 * A34;
+    A34x = -Lu*sin(q3);
+    A34y =  Lu*cos(q3);
+    A34z =  0;
+    A34R = [cos(q3), -sin(q3), 0;
+            sin(q3),  cos(q3), 0;
+                  0,        0, 1];
+    A34  = [A34R, [A34x A34y A34z]'; ROW4];
+    HTs.A04 = HTs.A03 * A34;
 
-        A45x = -S*sin(q4);
-        A45y =  S*cos(q4);
-        A45z =  0;
-        A45R = [0, -sin(q4),  cos(q4);
-                0,  cos(q4),  sin(q4);
-               -1,        0,        0];
-        A45  = [A45R, [A45x A45y A45z]'; ROW4];
-        HTs.A05 = HTs.A04 * A45;
+    A45x = -S*sin(q4);
+    A45y =  S*cos(q4);
+    A45z =  0;
+    A45R = [0, -sin(q4),  cos(q4);
+            0,  cos(q4),  sin(q4);
+           -1,        0,        0];
+    A45  = [A45R, [A45x A45y A45z]'; ROW4];
+    HTs.A05 = HTs.A04 * A45;
 
-        A56x = -S*sin(q5);
-        A56y =  S*cos(q5);
-        A56z =  0;
-        A56R = [cos(q5), 0,  sin(q5);
-                sin(q5), 0, -cos(q5);
-                      0, 1,        0];
-        A56  = [A56R, [A56x A56y A56z]'; ROW4];
-        HTs.A06 = HTs.A05 * A56;
+    A56x = -S*sin(q5);
+    A56y =  S*cos(q5);
+    A56z =  0;
+    A56R = [cos(q5), 0,  sin(q5);
+            sin(q5), 0, -cos(q5);
+                  0, 1,        0];
+    A56  = [A56R, [A56x A56y A56z]'; ROW4];
+    HTs.A06 = HTs.A05 * A56;
 
-        % ___ MID-BEGIN ___ %
-        A6Hx = H/2;
-        A6Hy =   0;
-        A6Hz =   0;
-        A6HR = eye(3);
-        A6H  = [A6HR, [A6Hx A6Hy A6Hz]'; ROW4];
-        HTs.A0H =  HTs.A06 * A6H;
-        % ___  MID-END  ___ %
+    % ___ MID-BEGIN ___ %
+    A6Hx = H/2;
+    A6Hy =   0;
+    A6Hz =   0;
+    A6HR = eye(3);
+    A6H  = [A6HR, [A6Hx A6Hy A6Hz]'; ROW4];
+    HTs.A0H =  HTs.A06 * A6H;
+    % ___  MID-END  ___ %
 
-        A67x =  H*cos(q6);
-        A67y =  H*sin(q6);
-        A67z =  0;
-        A67R = [cos(q6), -sin(q6), 0;
-                sin(q6),  cos(q6), 0;
-                      0,        0, 1];
-        A67  = [A67R, [A67x A67y A67z]'; ROW4];
-        HTs.A07 = HTs.A06 * A67;
+    HTs.A0ER = A0ER;
+    HTs.A012 = HTs.A0ER * AE12;
 
-        A78x =  0;
-        A78y =  0;
-        A78z =  S;
-        A78R = [cos(q7),  0, -sin(q7);
-                sin(q7),  0,  cos(q7);
-                      0, -1,       0];
-        A78  = [A78R, [A78x A78y A78z]'; ROW4];
-        HTs.A08 = HTs.A07 * A78;
+    A1211x =  S*sin(q12);
+    A1211y =  S*cos(q12);
+    A1211z =  0;
+    A1211R = [0, sin(q12), -cos(q12);
+              0, cos(q12),  sin(q12);
+              1,        0,         0];
+    A1211  = [A1211R, [A1211x A1211y A1211z]'; ROW4];
+    HTs.A011  = HTs.A012 * A1211;
 
-        A89x =  S*sin(q8);
-        A89y = -S*cos(q8);
-        A89z =  0;
-        A89R = [0, -sin(q8), -cos(q8);
-                0,  cos(q8), -sin(q8);
-                1,        0,       0];
-        A89  = [A89R, [A89x A89y A89z]'; ROW4];
-        HTs.A09 = HTs.A08 * A89;
+    A1110x =  Ll*sin(q11);
+    A1110y =  Ll*cos(q11);
+    A1110z =  0;
+    A1110R = [cos(q11), sin(q11), 0;
+             -sin(q11), cos(q11), 0;
+                     0,        0, 1];
+    A1110  = [A1110R, [A1110x A1110y A1110z]'; ROW4];
+    HTs.A010  = HTs.A011 * A1110;
 
-        A910x =  Lu*sin(q9);
-        A910y = -Lu*cos(q9);
-        A910z =  0;
-        A910R = [cos(q9), -sin(q9), 0;
-                 sin(q9),  cos(q9), 0;
-                 0,              0, 1];
-        A910  = [A910R, [A910x A910y A910z]'; ROW4];
-        HTs.A010 = HTs.A09 * A910;
+    A109x =  Lu*sin(q10);
+    A109y =  Lu*cos(q10);
+    A109z =  0;
+    A109R = [cos(q10), sin(q10), 0;
+            -sin(q10), cos(q10), 0;
+                   0,       0,   1];
+    A109  = [A109R, [A109x A109y A109z]'; ROW4];
+    HTs.A09  = HTs.A010 * A109;
 
-        A1011x =  Ll*sin(q10);
-        A1011y = -Ll*cos(q10);
-        A1011z =  0;
-        A1011R = [cos(q10), -sin(q10), 0;
-                  sin(q10),  cos(q10), 0;
-                         0,         0, 1];
-        A1011  = [A1011R, [A1011x A1011y A1011z]'; ROW4];
-        HTs.A011 = HTs.A010 * A1011;
+    A98x = S*sin(q9);
+    A98y = S*cos(q9);
+    A98z = 0;
+    A98R = [0, sin(q9),  cos(q9);
+            0, cos(q9), -sin(q9);
+           -1,       0,       0];
+    A98  = [A98R, [A98x A98y A98z]'; ROW4];
+    HTs.A08  = HTs.A09 * A98;
 
-        A1112x =  S*sin(q11);
-        A1112y = -S*cos(q11);
-        A1112z =  0;
-        A1112R = [0, -sin(q11), cos(q11);
-                  0,  cos(q11), sin(q11);
-                 -1,         0,        0];
-        A1112  = [A1112R, [A1112x A1112y A1112z]'; ROW4];
-        HTs.A012 = HTs.A011 * A1112;
-
-        A12Ex =  0;
-        A12Ey =  0;
-        A12Ez =  0;
-        A12ER = [0, -sin(q12), -cos(q12);
-                 0,  cos(q12), -sin(q12);
-                 1,         0,         0];
-        A12E  = [A12ER, [A12Ex A12Ey A12Ez]'; ROW4];
-        HTs.A0ER = HTs.A012 * A12E;
-    elseif params.mode == 0     % BOTH FIXED
-        HTs.A0EL = A0EL;
-        HTs.A01 = HTs.A0EL * AE1;
-
-        A12x = -S*sin(q1);
-        A12y =  S*cos(q1);
-        A12z =  0;
-        A12R = [0, -sin(q1), -cos(q1);
-                0,  cos(q1), -sin(q1);
-                1,        0,        0];
-        A12  = [A12R, [A12x A12y A12z]'; ROW4];
-        HTs.A02 = HTs.A01 * A12;
-
-        A23x = -Ll*sin(q2);
-        A23y =  Ll*cos(q2);
-        A23z =  0;
-        A23R = [cos(q2), -sin(q2), 0;
-                sin(q2),  cos(q2), 0;
-                      0,        0, 1];
-        A23  = [A23R, [A23x A23y A23z]'; ROW4];
-        HTs.A03 = HTs.A02 * A23;
-
-        A34x = -Lu*sin(q3);
-        A34y =  Lu*cos(q3);
-        A34z =  0;
-        A34R = [cos(q3), -sin(q3), 0;
-                sin(q3),  cos(q3), 0;
-                      0,        0, 1];
-        A34  = [A34R, [A34x A34y A34z]'; ROW4];
-        HTs.A04 = HTs.A03 * A34;
-
-        A45x = -S*sin(q4);
-        A45y =  S*cos(q4);
-        A45z =  0;
-        A45R = [0, -sin(q4),  cos(q4);
-                0,  cos(q4),  sin(q4);
-               -1,        0,        0];
-        A45  = [A45R, [A45x A45y A45z]'; ROW4];
-        HTs.A05 = HTs.A04 * A45;
-
-        A56x = -S*sin(q5);
-        A56y =  S*cos(q5);
-        A56z =  0;
-        A56R = [cos(q5), 0,  sin(q5);
-                sin(q5), 0, -cos(q5);
-                      0, 1,        0];
-        A56  = [A56R, [A56x A56y A56z]'; ROW4];
-        HTs.A06 = HTs.A05 * A56;
-
-        % ___ MID-BEGIN ___ %
-        A6Hx = H/2;
-        A6Hy =   0;
-        A6Hz =   0;
-        A6HR = eye(3);
-        A6H  = [A6HR, [A6Hx A6Hy A6Hz]'; ROW4];
-        HTs.A0H =  HTs.A06 * A6H;
-        % ___  MID-END  ___ %
-
-        HTs.A0ER = A0ER;
-        HTs.A012 = HTs.A0ER * AE12;
-
-        A1211x =  S*sin(q12);
-        A1211y =  S*cos(q12);
-        A1211z =  0;
-        A1211R = [0, sin(q12), -cos(q12);
-                  0, cos(q12),  sin(q12);
-                  1,        0,         0];
-        A1211  = [A1211R, [A1211x A1211y A1211z]'; ROW4];
-        HTs.A011  = HTs.A012 * A1211;
-
-        A1110x =  Ll*sin(q11);
-        A1110y =  Ll*cos(q11);
-        A1110z =  0;
-        A1110R = [cos(q11), sin(q11), 0;
-                 -sin(q11), cos(q11), 0;
-                         0,        0, 1];
-        A1110  = [A1110R, [A1110x A1110y A1110z]'; ROW4];
-        HTs.A010  = HTs.A011 * A1110;
-
-        A109x =  Lu*sin(q10);
-        A109y =  Lu*cos(q10);
-        A109z =  0;
-        A109R = [cos(q10), sin(q10), 0;
-                -sin(q10), cos(q10), 0;
-                       0,       0,   1];
-        A109  = [A109R, [A109x A109y A109z]'; ROW4];
-        HTs.A09  = HTs.A010 * A109;
-
-        A98x = S*sin(q9);
-        A98y = S*cos(q9);
-        A98z = 0;
-        A98R = [0, sin(q9),  cos(q9);
-                0, cos(q9), -sin(q9);
-               -1,       0,       0];
-        A98  = [A98R, [A98x A98y A98z]'; ROW4];
-        HTs.A08  = HTs.A09 * A98;
-
-        A87x =  S*sin(q8);
-        A87y =  S*cos(q8);
-        A87z =  0;
-        A87R = [cos(q8), 0, -sin(q8);
-               -sin(q8), 0, -cos(q8);
-                      0, 1,        0];
-        A87  = [A87R, [A87x A87y A87z]'; ROW4];
-        HTs.A07  = HTs.A08 * A87;
-
-        % ___ MID-BEGIN ___ %
-        A7Hx = -H/2;
-        A7Hy =    0;
-        A7Hz =    0;
-        A7HR = eye(3);
-        A7H  = [A7HR, [A7Hx A7Hy A7Hz]'; ROW4];
-        % ___  MID-END  ___ %
-        
-    elseif params.mode == -1     % RIGHT FIXED
-        HTs.A0ER = A0ER;
-        HTs.A012 = HTs.A0ER * AE12;
-        
-        A1211x =  S*sin(q12);
-        A1211y =  S*cos(q12);
-        A1211z =  0;
-        A1211R = [0,  sin(q12),  -cos(q12);
-                  0,  cos(q12),   sin(q12);
-                  1,         0,          0];
-        A1211  = [A1211R, [A1211x A1211y A1211z]'; ROW4];
-        HTs.A011  = HTs.A012 * A1211;
-
-        A1110x =  Ll*sin(q11);
-        A1110y =  Ll*cos(q11);
-        A1110z =  0;
-        A1110R = [cos(q11), sin(q11), 0;
-                 -sin(q11), cos(q11), 0;
-                         0,        0, 1];
-        A1110  = [A1110R, [A1110x A1110y A1110z]'; ROW4];
-        HTs.A010  = HTs.A011 * A1110;
-
-        A109x =  Lu*sin(q10);
-        A109y =  Lu*cos(q10);
-        A109z =  0;
-        A109R = [cos(q10), sin(q10), 0;
-                -sin(q10), cos(q10), 0;
-                       0,       0,   1];
-        A109  = [A109R, [A109x A109y A109z]'; ROW4];
-        HTs.A09  = HTs.A010 * A109;
-
-        A98x = S*sin(q9);
-        A98y = S*cos(q9);
-        A98z = 0;
-        A98R = [0, sin(q9),  cos(q9);
-                0, cos(q9), -sin(q9);
-               -1,       0,       0];
-        A98  = [A98R, [A98x A98y A98z]'; ROW4];
-        HTs.A08  = HTs.A09 * A98;
-
-        A87x =  S*sin(q8);
-        A87y =  S*cos(q8);
-        A87z =  0;
-        A87R = [cos(q8), 0, -sin(q8);
-               -sin(q8), 0, -cos(q8);
-                      0, 1,        0];
-        A87  = [A87R, [A87x A87y A87z]'; ROW4];
-        HTs.A07  = HTs.A08 * A87;
-
-        % ___ MID-BEGIN ___ %
-        A7Hx = -H/2;
-        A7Hy =    0;
-        A7Hz =    0;
-        A7HR = eye(3);
-        A7H  = [A7HR, [A7Hx A7Hy A7Hz]'; ROW4];
-        HTs.A0H =  HTs.A07 * A7H;
-        % ___  MID-END  ___ %
-
-        A76x = -H*cos(q7);
-        A76y =  H*sin(q7);
-        A76z =  0;
-        A76R = [cos(q7), sin(q7), 0;
-               -sin(q7), cos(q7), 0;
-                      0,       0, 1];
-        A76  = [A76R, [A76x A76y A76z]'; ROW4];
-        HTs.A06  = HTs.A07 * A76;
-
-        A65x =  0;
-        A65y =  0;
-        A65z =  S;
-        A65R = [cos(q6),  0, sin(q6);
-               -sin(q6),  0, cos(q6);
-                      0, -1,       0];
-        A65  = [A65R, [A65x A65y A65z]'; ROW4];
-        HTs.A05  = HTs.A06 * A65;
-
-        A54x = -S*sin(q5);
-        A54y = -S*cos(q5);
-        A54z =  0;
-        A54R = [0,  sin(q5), -cos(q5);
-                0,  cos(q5),  sin(q5);
-                1,        0,        0];
-        A54  = [A54R, [A54x A54y A54z]'; ROW4];
-        HTs.A04  = HTs.A05 * A54;
-
-        A43x = -Lu*sin(q4);
-        A43y = -Lu*cos(q4);
-        A43z =  0;
-        A43R = [cos(q4),  sin(q4), 0;
-               -sin(q4),  cos(q4), 0;
-                      0,        0, 1];
-        A43  = [A43R, [A43x A43y A43z]'; ROW4];
-        HTs.A03  = HTs.A04 * A43;
-
-        A32x = -Ll*sin(q3);
-        A32y = -Ll*cos(q3);
-        A32z =  0;
-        A32R = [cos(q3), sin(q3), 0;
-               -sin(q3), cos(q3), 0;
-                      0,       0, 1];
-        A32  = [A32R, [A32x A32y A32z]'; ROW4];
-        HTs.A02  = HTs.A03 * A32;
-
-        A21x = -S*sin(q2);
-        A21y = -S*cos(q2);
-        A21z =  0;
-        A21R = [0,  sin(q2),  cos(q2);
-                0,  cos(q2), -sin(q2);
-               -1,        0,        0];
-        A21  = [A21R, [A21x A21y A21z]'; ROW4];
-        HTs.A01  = HTs.A02 * A21;
-
-        A1Ex =  0;
-        A1Ey =  0;
-        A1Ez =  0;
-        A1ER = [0, sin(q1), -cos(q1);
-                0, cos(q1),  sin(q1);
-                1,       0,        0];
-        A1E  = [A1ER, [A1Ex A1Ey A1Ez]'; ROW4];
-        HTs.A0EL = HTs.A01 * A1E;
-    end
+    A87x =  S*sin(q8);
+    A87y =  S*cos(q8);
+    A87z =  0;
+    A87R = [cos(q8), 0, -sin(q8);
+           -sin(q8), 0, -cos(q8);
+                  0, 1,        0];
+    A87  = [A87R, [A87x A87y A87z]'; ROW4];
+    HTs.A07  = HTs.A08 * A87;
 end
