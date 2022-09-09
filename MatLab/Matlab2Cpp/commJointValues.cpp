0cb742269b664ac97ce46a5350722aaa2cbfcd47
Darcy Byrne <darcybyrne3@gmail.com>
2022-09-09 13:39:29 +1000

    Send joint variables packaged as a string

--- MatLab/Matlab2Cpp/commJointValues.cpp
+++ MatLab/Matlab2Cpp/commJointValues.cpp
@@ -24,110 +24,139 @@ using matlab::mex::ArgumentList;    //
 
 class MexFunction : public matlab::mex::Function {
 
-    public: 
-    void operator()(ArgumentList outputs, ArgumentList inputs) {
-        int argc = checkArguments(outputs, inputs);
+    public:
+        void operator()(ArgumentList outputs, ArgumentList inputs) {
+            int argc = checkArguments(outputs, inputs);
+            
+            if(setSocketIPv4(mexIn2Str(inputs[0]),stoi(mexIn2Str(inputs[1]))) <= 0) {
+                throwError("\nInvalid address || Address not supported\n");
+            };
 
-        std::string ipAddressStr = mexIn2Str(inputs[0]);
-        const char * IP_ADDRESS = ipAddressStr.c_str();
-        int PORT = stoi(mexIn2Str(inputs[1]));
+            const int sockFD = socket(server_address.sin_family, SOCK_STREAM, 0);
+            if (sockFD < 0) {
+                throwError("\nSocket creation error \n");
+            };
+            
+            const int clientFD = connect(sockFD, (struct sockaddr*)&server_address, sizeof(server_address));
+            if (clientFD < 0) {
+                throwError("\nConnection Failed \n");
+            };
 
-        int sock = 0, valread, client_fd, n;
-        struct sockaddr_in serv_addr;
-        char* hello = "Hello from client\n";
+            if(argc > 2) {
+                std::string messageStr = createMessageStr(inputs[2]);
+                char* messageChr = &messageStr[0];
+                send(sockFD, messageChr, strlen(messageChr), 0);
+                close(sockFD);
+                close(clientFD);
+            } else {
+                char* messageChr = (char*) "GET";
+                send(sockFD, messageChr, strlen(messageChr), 0);
 
-        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-            throwError("\n Socket creation error \n");
+                std::string reply(32, ' ');
+                auto bytes_recv = recv(sockFD, &reply.front(), reply.size(), 0);
+                if (bytes_recv == -1) {
+                    std::cerr << "Error while receiving bytes\n";
+                }
+
+                std::cout << "\nClient recieved: " << reply << std::endl;
+                close(sockFD);
+                close(clientFD);
+            }
+
+            outputs[0] = inputs[0];
         }
-    
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(PORT);
-    
-        // Convert IPv4 and IPv6 addresses from text to binary form
-        if (inet_pton(AF_INET, IP_ADDRESS, &serv_addr.sin_addr) <= 0) {
-            throwError("\nInvalid address/ Address not supported \n");
-        }
-    
-        if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-            throwError("\nConnection Failed \n");
-        }
-
-        send(sock, hello, strlen(hello), 0);
-        mprint("Hello message sent\n");
-
-        std::string reply(32, ' ');
-        auto bytes_recv = recv(sock, &reply.front(), reply.size(), 0);
-        if (bytes_recv == -1) {
-            std::cerr << "Error while receiving bytes\n";
-        }
-
-        std::cout << "\nClient recieved: " << reply << std::endl;
-        close(sock);
-        close(client_fd);
-
-        outputs[0] = inputs[0];
-    }
 
     private:
+        struct sockaddr_in server_address;
 
-    /**
-     *  checkArguments
-     *      standard first in MEX function
-     */ 
-    int checkArguments(ArgumentList outputs, ArgumentList inputs) {
-        if(inputs.size() > 3 || inputs.size() < 2) {
-            mprint("Fetch Joint Variables: <IP_ADDRESS>, <PORT> \n");
-            mprint("Send  Joint Variables: <IP_ADDRESS>, <PORT>, Joint_Variables[] \n");
-            throwError("Incorrect number of arguments");
+        /** 
+         *  setSocketIPv4
+         *      inet_pton() -> Converts IPv4 address from char[] to binary form
+         */
+        int setSocketIPv4(std::string ipAddressStr, int portNumber) {
+            const char *IP_ADDRESS = ipAddressStr.c_str();
+            const int pass = inet_pton(AF_INET, IP_ADDRESS, &server_address.sin_addr);
+            server_address.sin_family = AF_INET;
+            server_address.sin_port = htons(portNumber);
+            return pass;
         }
 
-        if( inputs[0].getType() != ArrayType::MATLAB_STRING || 
-            inputs[1].getType() != ArrayType::MATLAB_STRING ) {
-            throwError("IP Address and Port Number should be CHAR Arrays.\nUse Double Quotes\n");
+        /** 
+         *  createMessageStr
+         */
+
+        std::string createMessageStr(matlab::data::TypedArray<double> inArray) {
+            std::ostringstream sstream;
+
+            sstream.precision(4);
+            sstream << "[";
+            for(double value : inArray) {
+                sstream << std::fixed << value << ", ";
+            }
+            std::string msg = sstream.str();
+            msg.erase(msg.end()-2, msg.end());
+            msg = msg + "]";
+
+            return msg;
         }
 
-        if(inputs.size() > 2 && inputs[2].getType() != ArrayType::DOUBLE) {
-            throwError("Joint Values should be...");
-            // getDimensions(), getNumberOfElements(), etc ...
+        /**
+         *  checkArguments
+         *      standard first in MEX function
+         */ 
+        int checkArguments(ArgumentList outputs, ArgumentList inputs) {
+            if(inputs.size() > 3 || inputs.size() < 2) {
+                mprint("Fetch Joint Variables: <IP_ADDRESS>, <PORT> \n");
+                mprint("Send  Joint Variables: <IP_ADDRESS>, <PORT>, Joint_Variables[] \n");
+                throwError("Incorrect number of arguments");
+            }
+
+            if( inputs[0].getType() != ArrayType::MATLAB_STRING || 
+                inputs[1].getType() != ArrayType::MATLAB_STRING ) {
+                throwError("IP Address and Port Number should be CHAR Arrays.\nUse Double Quotes\n");
+            }
+
+            if(inputs.size() > 2 && inputs[2].getType() != ArrayType::DOUBLE) {
+                throwError("Joint Values should be...");
+                // getDimensions(), getNumberOfElements(), etc ...
+            }
+            
+            return inputs.size();
+        }
+
+        /**
+         *  mprint()
+         *      compacting the method of displaying MATLAB text
+         *      ArrayFactory - Comes from `matlab::data`
+         *                   - Generates `matlab::data::Array` objects
+         * 
+         *  Example:
+         *      mprint( std::to_string() + " TEST" );
+         */
+        void mprint(std::string text) {
+            std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+            ArrayFactory factory;
+            matlabPtr->feval("fprintf", 0, std::vector<Array>({ factory.createScalar(text) }));
+        }
+
+        /**
+         *  throwError()
+         *      compacting the method of throwing a MATLAB error
+         *      ArrayFactory - Comes from `matlab::data`
+         *                   - Generates `matlab::data::Array` objects
+         */
+        void throwError(std::string errorText) {
+            std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+            ArrayFactory factory; 
+            matlabPtr->feval("error", 0, std::vector<Array>({ factory.createScalar(errorText) }));
         }
         
-        return inputs.size();
-    }
-
-    /**
-     *  mprint()
-     *      compacting the method of displaying MATLAB text
-     *      ArrayFactory - Comes from `matlab::data`
-     *                   - Generates `matlab::data::Array` objects
-     * 
-     *  Example:
-     *      mprint( std::to_string() + " TEST" );
-     */
-    void mprint(std::string text) {
-        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
-        ArrayFactory factory;
-        matlabPtr->feval("fprintf", 0, std::vector<Array>({ factory.createScalar(text) }));
-    }
-
-    /**
-     *  throwError()
-     *      compacting the method of throwing a MATLAB error
-     *      ArrayFactory - Comes from `matlab::data`
-     *                   - Generates `matlab::data::Array` objects
-     */
-    void throwError(std::string errorText) {
-        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
-        ArrayFactory factory; 
-        matlabPtr->feval("error", 0, std::vector<Array>({ factory.createScalar(errorText) }));
-    }
-    
-    /**
-     *  mexIn2Str()
-     *      pass in an object from the `ArgumentList[]` array
-     *      return string
-     */ 
-    std::string mexIn2Str(matlab::data::TypedArray<matlab::data::MATLABString> input) {
-        return input[0];
-    }
-
+        /**
+         *  mexIn2Str()
+         *      pass in an object from the `ArgumentList[]` array
+         *      return string
+         */ 
+        std::string mexIn2Str(matlab::data::TypedArray<matlab::data::MATLABString> input) {
+            return input[0];
+        }
 };
