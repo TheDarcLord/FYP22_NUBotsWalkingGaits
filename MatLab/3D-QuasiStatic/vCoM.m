696cdb658ba41ed8d397223c84c9105f6a5e198d
Darcy Byrne <darcybyrne3@gmail.com>
2022-10-17 23:26:07 +1100

    Update vCoM.m
    Norm(vCoM) -> More fluid. Components can be in any direction

--- MatLab/3D-QuasiStatic/vCoM.m
+++ MatLab/3D-QuasiStatic/vCoM.m
@@ -2,7 +2,7 @@ function [c, ceq] = vCoM(q, rCoMi, p)
 %VCOM Summary of this function goes here
 %   Detailed explanation goes here
     dt  = p.timestp;
-    c   = abs(rCoM(q,p) - rCoMi)./dt - [0.1; 0.1; 0.1];
+    c   = norm((rCoM(q,p) - rCoMi)./dt) - 0.1;
     ceq = [];
 end
 
