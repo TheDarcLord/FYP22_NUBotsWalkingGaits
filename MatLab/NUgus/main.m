59869d0111ba2bb5b825c70f602b14aa953fdc4f
Darcy Byrne <darcybyrne3@gmail.com>
2022-10-20 17:04:46 +1100

    Update NUgus

--- MatLab/NUgus/main.m
+++ MatLab/NUgus/main.m
@@ -3,9 +3,9 @@ close all
 clc 
 
 %% Video & Time Parameters
-    params.framerate  = 20;                             % FPS
+    params.framerate  = 25;                             % FPS
     params.timestp    = params.framerate^(-1);          % Seconds
-    model.tspan       = 0 : params.timestp : 120;         % [ time ]
+    model.tspan       = 0 : params.timestp : 120;       % [ time ]
     model.timeHrzn    = 0;                            % Seconds
     model.Nl          = model.timeHrzn / params.timestp; % INTEGER
  % Physical Parameters - Affect CoM or FKM   |
@@ -24,15 +24,14 @@ clc
     params.hip2waist  = [-0.06895; 0.04625; -0.0545];
     params.ServoSize  = 0.05;     % m    - Approximation/Spacing
  % Masses
-    params.mass.fibula = 0.1719;        % Paired with `tibia`
-    params.mass.femur  = 0.3423;        % Thigh Bone
-    params.mass.joint  = 0.300  + 0.7;        % Knee Bone / Joints
-    params.mass.pelvis = 2.9549 + 697.6 + 753.5;         % Waist
-    params.mass.foot   = 0.2023 + 0.5;        % Foot
+    params.mass.fibula = 0.1719;                    % Paired with `tibia`
+    params.mass.femur  = 0.3423;                    % Thigh Bone
+    params.mass.joint  = 0.300  + 0.7;              % Knee Bone / Joints
+    params.mass.pelvis = 2.9549 + 697.6 + 753.5;    % Waist
+    params.mass.foot   = 0.2023 + 0.5;              % Foot
 
 %% Model setup
  % Steps
-    % Stepping mode... Array!? ... ?
     model.mode     = zeros(1,length(model.tspan));
     params.mode    = -1;       % LEFT  FIXED - FKM T16
     %                 0;       % BOTH  FIXED - FKM T1H T6H
@@ -70,7 +69,6 @@ clc
                    -1;  % θ₁₁
                     1]; % θ₁₂
 
-
     model.q(:,1)      = model.q0;
     model.xe(:,1)     = k(model.q0, params);
     model.r0CoMg(:,1) = rCoM(model.q0,params);
@@ -96,11 +94,13 @@ clc
         zlabel('{\bfY} (metres)');
         [~] = plotRobot(1,model,params);
     % +-+-+-+-+-+-+-+-+-+-+-+
-    for i=1:5
-        commJointValues("10.0.0.127","10013",...
-            [webotsMod.*model.q(:,1); arms./(5 - i)]...
-        );
-    end
+%     for i=1:5
+%         commJointValues("10.0.0.127","10013",...
+%             [webotsMod.*model.q(:,1); arms./(6 - i)]...
+%         );
+%     end
+    % +-+-+-+-+-+-+-+-+-+-+-+
+
 
 %% Generate Trajectory
     model.glbTrj = ...
@@ -113,18 +113,17 @@ clc
     gradFUNC = @(A,B) (B(2) - A(2)) ...
                      /(B(1) - A(1));  % Gradient -> ∇
     % Initialise variables
-    Nl       = model.Nl;              % N# INTEGER Future Indexes
-    stpLngth = params.StepLength;     % Step Size:   m
-    Q        = model.glbTrj;          %         Q:  [x y z]ᵀ
-    Qstep    = zeros(6,length(model.glbTrj)); % Qstep:  [x 0 z]ᵀ
-    r        = (params.HipWidth/2) + 0.05;     % Radius of Circle
+    stpLngth = params.StepLength;              % Step Size:   m
+    Q        = model.glbTrj;                   % Q:         [x y z]ᵀ
+    Qstep    = zeros(6,length(model.glbTrj));  % Qstep:     [xe]
+    r        = (params.HipWidth/2) + 0.02;     % Radius of Circle + shim
     STEP     = params.mode;           % DEFINE MODE:  1 RIGHT Step 
                                       %              -1 LEFT  Step
-    accuDist = 0;                     % Accumulated Distance
-    model.pREF(:,1) = Q([1 3],1);     % Last Foot Step -> Traj Start Point
-    A        = model.glbTrj(:,1);     % A = [x₁ y₁ z₁]ᵀ
-    t_begin  = 2;                     % Index of Step Beginning
-    model.TBE = eye(4);               % HomoTrans: Base -> End Effector
+    accuDist        = 0;                 % Accumulated Distance
+    model.pREF(:,1) = Q([1 3],1);        % Last Foot Step -> TrajStartPoint
+    A               = model.glbTrj(:,1); % A = [x₁ y₁ z₁]ᵀ
+    t_begin         = 2;                 % Index of Step Beginning
+    model.TBE       = eye(4);            % HomoTrans: Base -> End Effector
     for i=2:length(model.tspan)
         tic
         accuDist = accuDist + norm(Q(:,i-1) - Q(:,i));
@@ -159,37 +158,18 @@ clc
                                        t_begin:t_end, ...
                                        model,params);
 
-
-
             for j=t_begin:t_end
                 jn = j - 1;
-                if jn < 1
-                    jn = 1;
-                end
-    
                 model.mode(:,j)     = params.mode;
                 xeSTAR = Qstep(:,j);
                 model.q(:,j)  = k_Inv(model.q(:,jn), ...
                                         xeSTAR, j, model, params);
                 [model.xe(:,j), model.TBE] = k(model.q(:,j), params);
-                commJointValues("10.0.0.127","10013",...
-                    [webotsMod.*model.q(:,j); arms]...
-                );
-
-            ROBOT_FRAME = figure(1);
-                cla(ROBOT_FRAME);
-                hold on
-                grid on
-                set(gca,'Color','#CCCCCC');
-                title("3D Model - ZMP Walking",'FontSize',12);
-                xlabel('{\bfZ} (metres)');
-                ylabel('{\bfX} (metres)');
-                zlabel('{\bfY} (metres)');
-                view(-145,50);
-                [~] = plotRobot(j,model,params);
-                TIME = t_begin:t_end;
-                plot3(Qstep(3,TIME),Qstep(1,TIME),Qstep(2,TIME),...
-                    'b-','LineWidth',2)
+                DEBUG(j,t_begin,t_end,Qstep,model,params);
+                % Send qᵢ
+%                 commJointValues("10.0.0.127","10013",...
+%                     [webotsMod.*model.q(:,j); arms]...
+%                 );
             end
 
             % CLEAN UP
@@ -228,16 +208,16 @@ clc
 
 %% RESEND q(t)
 
-for i=1:5
-    commJointValues("10.0.0.127","10013",...
-        [webotsMod.*model.q(:,1); arms]...
-    );
-end
-for i=1:length(model.tspan)
-        commJointValues("10.0.0.127","10013",...
-        [webotsMod.*model.q(:,i); arms]...
-    );
-end
+% for i=1:5
+%     commJointValues("10.0.0.127","10013",...
+%         [webotsMod.*model.q(:,1); arms]...
+%     );
+% end
+% for i=1:length(model.tspan)
+%         commJointValues("10.0.0.127","10013",...
+%         [webotsMod.*model.q(:,i); arms./(6 - i)]...
+%     );
+% end
 
 %% VIDEO
 videoWriterObj           = VideoWriter('3D_Step.mp4','MPEG-4');
