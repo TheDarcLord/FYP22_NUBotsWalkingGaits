1c826f9b7bab5ca994289ff0fb541b565079a20b
Darcy Byrne <darcybyrne3@gmail.com>
2022-11-11 16:29:21 +1100

    Kill single NUgus

--- NUgus/trajGen_cir.m
+++
@@ -1,8 +0,0 @@
-function Q = trajGen_cir(tspan, init)
-    omega = pi/23;
-    r = 0.5;
-
-    Q = init + [r*sin(omega*tspan);
-                zeros(size(tspan));
-                r*cos(omega*tspan) - r];
-end
